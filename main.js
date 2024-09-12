72fc1bc8f2942a098fc146a0776343ad684fc393
yohejazi11 <yohejazi11@gmail.com>
2024-09-12 09:44:50 +0300

    edite

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 
-const name=document.getElementById('');
+const namee=document.getElementById('name');
 const number=document.getElementById('number');
 
-const email=document.getElementById('');
-const web=document.getElementById('');
+const email=document.getElementById('email');
+const adress=document.getElementById('address');
 
+const web=document.getElementById('web');
+const image=document.getElementById('avaterimage');
 
-
-setInterval(() => {
-    const isHovered = window.getComputedStyle(name).backgroundColor === 'rgb(144, 238, 144)';
-    if (isHovered) {
-        name.textContent = ''
-    } else {
-        hoverableDiv.textContent = '';
+function update(id){
+    if(id==="number"){
+        number.textContent=`+966503531981`;
     }
-
-    
-}, 100);
+    if(id==="email"){
+        email.textContent=`yohejazi@gmail.com`;
+    }
+    if(id==="address"){
+        adress.textContent=`riyadh azizyah`;
+    }
+    if(id==="web"){
+        web.textContent=`yosef.com`;
+    }
+    if(id==="number"){
+        number.textContent=`+966503531981`;
+    }
+    if(id==="avaterimage"){
+        image.setAttribute('src','https://images.pexels.com/photos/27835751/pexels-photo-27835751/free-photo-of-a-tree-with-green-apples-on-it.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load');
+    }
+    if(id==="name"){
+        namee.textContent=`YOUSEF HEJAZI`;
+    }
+}
